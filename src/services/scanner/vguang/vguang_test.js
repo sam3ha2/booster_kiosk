c1ec36da728d9ea12cc949aa796591c162a12590
follow <follow@boosteron.co.kr>
2024-11-08 10:30:51 +0900

    스캐너 테스트

---
+++ src/services/scanner/vguang/vguang_test.js
@@ -0,0 +1,14 @@
+const VguangScanner = require("./VguangScanner");
+
+const scanner = new VguangScanner({ mode: "tx400" });
+
+let is = true;
+
+scanner.on("data", data => {
+  if (is) {
+    is = false;
+    console.log(data);
+    scanner.beep(1);
+    setTimeout(() => is = true, 800);
+  }
+});
